e8cb78364a3b4c16045b248e72d664aea15f85fc
ivancho2016 <leodanhijo@gmail.com>
2017-03-14 11:49:48 -0600

    sitio completo
    solo falta minificar imagenes

--- app/procesar-mail.php
+++ app/procesar-mail.php
@@ -3,28 +3,31 @@
 $errores = '';
 $enviado = '';
 
-if (isset($_POST['submit'])) {
+if (isset($_POST['enviar'])) {
 	$nombre = $_POST['nombre'];
 	$email = $_POST['email'];
 	$mensaje = $_POST['mensaje'];
 
+	//nombre
 	if (!empty($nombre)) {
 		$nombre = trim($nombre);
 		$nombre = filter_var($nombre, FILTER_SANITIZE_STRING);
 	} else {
-		$errores .= 'Por favor ingresa un nombre <br/>';
+		$errores .= '<div>Por favor ingresa tu nombre</div> <br>';
 	}
 
+	//email
 	if (!empty($email)) {
 		$email = filter_var($email, FILTER_SANITIZE_EMAIL);
 
 		if(!filter_var($email, FILTER_VALIDATE_EMAIL)) {
-			$errores .= "Por favor ingresa un email valido <br/>";
+			$errores .= "Por favor ingresa un correo valido <br/>";
 		}
 	} else {
-		$errores .= 'Por favor ingresa un email <br/>';
+		$errores .= 'Por favor ingresa un correo <br/>';
 	}
 
+	//mensaje
 	if (!empty($mensaje)) {
 		$mensaje = htmlspecialchars($mensaje);
 		$mensaje = trim($mensaje);
@@ -33,20 +36,39 @@ if (isset($_POST['submit'])) {
 		$errores .= 'Por favor ingresa el mensaje <br/>';
 	}
 
+	//mensaje mensaje
 	if (!$errores) {
-		$enviar_a = 'leodan_hijo@hotmail.com';
-		$asunto = 'email enviado desde mi página web';
-		$mensaje = "De:  $nombre \n";
-		$mensaje .= "email:  $email \n";
-		$mensaje .= "Mensaje: " . $mensaje;
+		$para = 'jgmuros-moviles@hotmail.es';
+		$titulo = 'Mensaje enviado desde murosacusticosgarcia.com.mx';
+		$mensajeFinal = "
+		  <p>Email desde el sitio web</p>
+		  <table>
+		    <tr>
+		      <td>Nombre: </td><td>$nombre</td>
+		    </tr>
+		    <tr>
+		      <td>Email: </td><td>$email</td>
+		    </tr>
+		    <tr>
+		      <td>Mensaje: </td><td>$mensaje</td>
+		    </tr>
+		  </table>
+		";
+		$cabeceras  = 'MIME-Version: 1.0' . "\r\n";
+		$cabeceras .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
 
-		mail($enviar_a, $asunto, $mensaje_preparado);
-		$enviado = true;
+		mail($para, $titulo, $mensajeFinal, $cabeceras);
+		//$enviado = true;
+		echo "<script language='javascript'>";
+		echo "alert('Enviado Correctamente. Gracias')";
+		echo "</script>";
+	} else {
+		echo "<script language='javascript'>";
+		echo "alert('Hubo un problema para envíar tu email, intenta más tarde. Gracias')";
+		echo "</script>";
 	}
-
 }
 
-// Esto llamara a la vista del index, que contiene el formulario
 require 'contactanos.html';
 
 ?>
